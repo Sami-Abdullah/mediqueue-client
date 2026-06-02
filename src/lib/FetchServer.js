d1341eb767c4da0ec26dde01681c6d62f99952ae
Sami Abdullah <sami38596@gmailcom>
2026-06-03 01:54:22 +0600

    last push

--- src/lib/FetchServer.js
+++ src/lib/FetchServer.js
@@ -4,7 +4,7 @@ export const getTutorDetails = async (id) => {
     const { token } = await auth.api.getToken({
       headers: await headers()
     })
-    const res = await fetch(`http://localhost:5000/tutors/${id}`, {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/tutors/${id}`, {
       method:"GET",
       headers: {
         authorization: `Bearer ${token}`,
